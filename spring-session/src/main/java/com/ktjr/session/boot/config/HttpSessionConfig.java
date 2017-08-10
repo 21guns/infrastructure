aeb8c4e700c313872985b3836f751363001c443b
jliu <liujun_0810@163.com>
2017-08-10 16:42:49 +0800

    feature:add spring session

---
+++ spring-session/src/main/java/com/ktjr/session/boot/config/HttpSessionConfig.java
@@ -0,0 +1,28 @@
+package com.ktjr.session.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+/**
+ * Created by ljj on 17/5/24.
+ */
+@Configuration
+public class HttpSessionConfig {
+
+    /**
+     * 通过header传递session ID
+     * @return
+     */
+    @Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
+
+//    @Bean( name = "springSessionDefaultRedisSerializer")
+    public RedisSerializer redisSerializer() {
+        return new FastJsonSessionSerializer();
+    }
+}
---
+++ spring-session/src/main/java/com/ktjr/session/domain/AuthInfo.java
@@ -0,0 +1,39 @@
+package com.ktjr.session.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by ljj on 17/5/24.
+ */
+public class AuthInfo implements Serializable {
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    private String userId;
+    private String userName;
+    private List<String> roles;
+
+}
+
