6c5a3b95b7fb5fd830dfa92eac20ac0068c33910
jliu <liujun_0810@sohu.com>
2017-09-12 18:09:30 +0800

    issue:add sever.session.timeout for redis

--- spring-session/src/main/java/com/guns21/session/boot/config/HttpSessionConfig.java
+++ spring-session/src/main/java/com/guns21/session/boot/config/HttpSessionConfig.java
@@ -1,22 +1,31 @@
 package com.guns21.session.boot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by ljj on 17/5/24.
  */
 @Configuration
 //@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 5)
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 5 * 356 * 24 * 60 * 60)
+@EnableRedisHttpSession
 public class HttpSessionConfig {
+    @Value("${server.session.timeout:1800}")
+    private Integer maxInactiveIntervalInSeconds;
 
+    @Autowired
+    private RedisOperationsSessionRepository sessionRepository;
     /**
      * 通过header传递session ID.
      */
@@ -29,6 +38,11 @@ public class HttpSessionConfig {
     @ConditionalOnMissingBean(RedisSerializer.class)
     public RedisSerializer springSessionDefaultRedisSerializer() {
         return new GenericJackson2JsonRedisSerializer();
+    }
+
+    @PostConstruct
+    private void afterPropertiesSet() {
+        sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
     }
 
 }
