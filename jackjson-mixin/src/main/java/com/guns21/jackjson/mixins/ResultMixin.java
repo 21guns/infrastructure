4762f3902ae6dcb972e76645a9c999001e0c62cc
jliu <liujun_0810@sohu.com>
2017-11-04 16:49:25 +0800

    issue:ignore page

--- jackjson-mixin/src/main/java/com/guns21/jackjson/mixins/ResultMixin.java
+++ jackjson-mixin/src/main/java/com/guns21/jackjson/mixins/ResultMixin.java
@@ -1,7 +1,10 @@
 package com.guns21.jackjson.mixins;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by jliu on 16/7/13.
  */
+@JsonIgnoreProperties({"page", "pageSize", "totals"})
 public interface ResultMixin {
 }
