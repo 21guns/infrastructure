aed33dab78a0968856965a06cf9e01ebe33b7fbe
jliu <liujun_0810@sohu.com>
2018-05-04 15:03:07 +0800

    issue:change commence warn message

--- authorization/src/main/java/com/guns21/authorization/security/HttpAuthenticationEntryPoint.java
+++ authorization/src/main/java/com/guns21/authorization/security/HttpAuthenticationEntryPoint.java
@@ -25,7 +25,7 @@ public class HttpAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException)
             throws IOException, ServletException {
-            log.warn("commence", authException);
+            log.warn("commence {}", authException.toString());
         ResponseUtils.writeResponse(response, Result.fail401(messageSourceAccessor.getMessage("com.guns21.security.message.login.commence","请登录")));
     }
 
