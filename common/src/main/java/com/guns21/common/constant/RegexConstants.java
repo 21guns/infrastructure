49d9ef3529809a9687b5c48b1a85efecf58ab9d2
jliu <liujun_0810@sohu.com>
2018-06-29 14:25:28 +0800

    issue:add mobild num start with 199

--- common/src/main/java/com/guns21/common/constant/RegexConstants.java
+++ common/src/main/java/com/guns21/common/constant/RegexConstants.java
@@ -3,7 +3,7 @@ package com.guns21.common.constant;
 public class RegexConstants {
     public static final String UUID_REGEX_32 = "[0-9a-z]{32}";
     public static final String UUID_REGEX_24 = "[0-9a-z]{24}";
-    public static final String MOBILE_REGEXP = "^[1]([3,5,7,8][0-9]{1}|59|58|88|89)[0-9]{8}$";
+    public static final String MOBILE_REGEXP = "^[1]([3,5,7,8,9][0-9]{1}|59|58|88|89)[0-9]{8}$";
     public static final String PASSWORD_REGEXP = "^[A-Za-z0-9\\^\\$\\.\\+\\*_@!#%&~=-]{6,32}$";
     public static final String ID_CARD_REGEXP = "(^\\d{15}$)|(^\\d{17}([0-9]|X)$)";
 }
