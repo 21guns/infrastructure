ffe9d5b2b5d5cc8546755a7ef62be8400a177d40
jliu <liujun_0810@sohu.com>
2019-04-17 15:36:05 +0800

    issue:collections not empty

--- authentication/src/main/java/com/guns21/authentication/boot/config/SecurityConfig.java
+++ authentication/src/main/java/com/guns21/authentication/boot/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.guns21.authentication.boot.config;
 
+import com.guns21.common.util.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -34,7 +35,7 @@ public class SecurityConfig {
         private String[] permitPages;
 
         public void setPages(List<String> permitPages) {
-            if (Objects.nonNull(permitPages)) {
+            if (ObjectUtils.nonEmpty(permitPages)) {
                 this.permitPages = permitPages.toArray(new String[0]);
             } else {
                 this.permitPages = null;
