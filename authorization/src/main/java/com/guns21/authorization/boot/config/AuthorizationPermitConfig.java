4406b4458e23707d37b7c5c04cc98205f3fb9bd5
jliu <liujun_0810@sohu.com>
2019-04-24 18:24:23 +0800

    fixed:permit all url exclude login/logout

--- authorization/src/main/java/com/guns21/authorization/boot/config/AuthorizationPermitConfig.java
+++ authorization/src/main/java/com/guns21/authorization/boot/config/AuthorizationPermitConfig.java
@@ -1,7 +1,6 @@
 package com.guns21.authorization.boot.config;
 
-import com.guns21.common.util.ObjectUtils;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,24 +8,21 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import java.util.List;
-import java.util.Objects;
-
 /**
- * com.guns21.security.auth.permit 和 com.guns21.security.permit的区别是前一个配置有security的各种filter
  * WebSecurity.IgnoredRequestConfigurer 直接就忽略，不对url追加spring security各种filter,不进行鉴权
- * http.authorizeRequests().antMatchers("/api/**").permitAll(); 会追加spring security各种filter，进行鉴权
+ * http.authorizeRequests().antMatchers("/api/**").permitAll(); 会追加spring security各种filter，不进行鉴权
  */
 @Configuration
 @EnableWebSecurity
 @Order(101)
-@ConfigurationProperties(prefix = "com.guns21.security.auth.permit")
 public class AuthorizationPermitConfig extends WebSecurityConfigurerAdapter {
-    private String[] permitPages;
+    @Value("${com.guns21.security.permit.authorize-any:false}")
+    private boolean permitAny;
+;
 
     @Override
     public void init(WebSecurity web) throws Exception {
-        if (Objects.nonNull(permitPages)) {
+        if (permitAny) {
             super.init(web);
         }
     }
@@ -34,15 +30,8 @@ public class AuthorizationPermitConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers(permitPages).permitAll()
+                .authorizeRequests().anyRequest().permitAll()
                 .and().csrf().disable();
-    }
-    public void setPages(List<String> permitPages) {
-        if (ObjectUtils.nonEmpty(permitPages)) {
-            this.permitPages = permitPages.toArray(new String[0]);
-        } else {
-            this.permitPages = null;
-        }
     }
 
 }
