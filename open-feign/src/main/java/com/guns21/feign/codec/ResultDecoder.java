45a0fb957d312e30c310df8307ef6686b12b6cfe
Liu Xiang <liuxiang@ktjr.com>
2019-07-19 14:09:36 +0800

    Add feign decoder for getting optiona/stream directly

--- open-feign/src/main/java/com/guns21/feign/codec/ResultDecoder.java
+++ open-feign/src/main/java/com/guns21/feign/codec/ResultDecoder.java
@@ -1,7 +1,39 @@
 package com.guns21.feign.codec;
 
+import com.guns21.data.domain.result.MessageResult;
+import feign.Response;
+import feign.jackson.JacksonDecoder;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 /**
  * Created by Liu Xiang on 2019-07-19.
  */
-public class ResultDecoder {
+public class ResultDecoder extends JacksonDecoder {
+    @Override
+    public Object decode(Response response, Type type) throws IOException {
+        Type shadeType = type;
+        if (Arrays.asList(Optional.class, Stream.class).contains(type.getClass())) {
+            shadeType = MessageResult.class;
+        }
+
+        Object result = super.decode(response, shadeType);
+        if (result instanceof MessageResult) {
+            MessageResult messageResult = (MessageResult) result;
+            if (Objects.equals(Optional.class, type)) {
+                return messageResult.optional();
+            }
+
+            if (Objects.equals(Stream.class, type)) {
+                return messageResult.stream();
+            }
+        }
+
+        return result;
+    }
 }
