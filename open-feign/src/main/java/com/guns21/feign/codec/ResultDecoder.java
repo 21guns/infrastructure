7b617b8943acff61bdc17ba86fa9fc7df54a4eac
Liu Xiang <liuxiang@ktjr.com>
2019-07-22 09:58:42 +0800

    Fix bug of parameterized types

--- open-feign/src/main/java/com/guns21/feign/codec/ResultDecoder.java
+++ open-feign/src/main/java/com/guns21/feign/codec/ResultDecoder.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guns21.data.domain.result.MessageResult;
 import feign.Response;
 import feign.jackson.JacksonDecoder;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -23,18 +25,37 @@ public class ResultDecoder extends JacksonDecoder {
     @Override
     public Object decode(Response response, Type type) throws IOException {
         Type shadeType = type;
-        if (Arrays.asList(Optional.class, Stream.class).contains(type.getClass())) {
-            shadeType = MessageResult.class;
+
+        Boolean returnOptionalOrStream = Stream.of(Optional.class, Stream.class).anyMatch(c -> {
+            if (type instanceof ParameterizedType) {
+                return Objects.equals(c, ((ParameterizedType) type).getRawType());
+            } else {
+                return Objects.equals(c, type);
+            }
+        });
+
+        if (returnOptionalOrStream) {
+            if (type instanceof ParameterizedType) {
+                ParameterizedType pt = (ParameterizedType) type;
+                if (Objects.equals(Stream.class, pt.getRawType())) {
+                    shadeType = ParameterizedTypeImpl.make(MessageResult.class,
+                            new Type[]{ ParameterizedTypeImpl.make(ArrayList.class, pt.getActualTypeArguments(), null)}, null);
+                } else {
+                    shadeType = ParameterizedTypeImpl.make(MessageResult.class, pt.getActualTypeArguments(), null);
+                }
+            } else {
+                shadeType = MessageResult.class;
+            }
         }
 
         Object result = super.decode(response, shadeType);
         if (result instanceof MessageResult) {
             MessageResult messageResult = (MessageResult) result;
-            if (Objects.equals(Optional.class, type)) {
+            if (type.getTypeName().startsWith(Optional.class.getTypeName())) {
                 return messageResult.optional();
             }
 
-            if (Objects.equals(Stream.class, type)) {
+            if (type.getTypeName().startsWith(Stream.class.getTypeName())) {
                 return messageResult.stream();
             }
         }
