d4cd801713956fd6bc66f57a4bbefe2af5c5f820
jliu <liujun_0810@sohu.com>
2019-12-05 17:37:55 +0800

    issue:add controller logging manual

--- spring-mvc/src/main/java/com/guns21/web/boot.config/ControllerLoggingConfig.java
+++ spring-mvc/src/main/java/com/guns21/web/boot.config/ControllerLoggingConfig.java
@@ -6,19 +6,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
+ * controller打印日志
+ * 1.引入 support 包
+ * 2.yml增加配置
+ * com:
+ *   guns21:
+ *     web:
+ *       request:
+ *         logging: true
+ * 3.yml或logback.xml中增加日志级别
+ * org.springframework.web.filter.CommonsRequestLoggingFilter = debug
  * @author jliu
  * @date 2019-11-20
  */
 @Configuration
 public class ControllerLoggingConfig {
     @Bean
-    @ConditionalOnProperty(name="logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter", havingValue="debug")
+    @ConditionalOnProperty(name="com.guns21.web.request.logging", havingValue="true")
     public CommonsRequestLoggingFilter requestLoggingFilter() {
         CommonsRequestLoggingFilter crlf = new CommonsRequestLoggingFilter();
         crlf.setIncludeClientInfo(true);
         crlf.setIncludeQueryString(true);
         crlf.setIncludePayload(true);
-        crlf.setMaxPayloadLength(1000);
+        crlf.setMaxPayloadLength(2000);
         return crlf;
     }
 }
