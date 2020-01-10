837fd7fb735aa40f0d03b43ce3f3200944f23c0f
jliu <liujun_0810@sohu.com>
2020-01-10 16:52:28 +0800

    feature:support enum for jackson

--- jackjson-mixin/src/main/java/com/guns21/jackjson/boot/config/JsonResponseConfig.java
+++ jackjson-mixin/src/main/java/com/guns21/jackjson/boot/config/JsonResponseConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -12,19 +13,27 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.guns21.common.enums.ValuableEnum;
 import com.guns21.jackjson.datatype.deser.StandrdLocalDateDeserializer;
 import com.guns21.jackjson.datatype.deser.StandrdLocalDateTimeDeserializer;
 import com.guns21.jackjson.datatype.deser.StandrdLocalTimeDeserializer;
 import com.guns21.jackjson.datatype.ser.StandrdLocalDateSerializer;
 import com.guns21.jackjson.datatype.ser.StandrdLocalDateTimeSerializer;
 import com.guns21.jackjson.datatype.ser.StandrdLocalTimeSerializer;
+import com.guns21.jackjson.deserializer.ValuableEnumDeserializer;
 import com.guns21.jackjson.http.converter.json.JsonResponseAwareJsonMessageConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.CollectionUtils;
@@ -35,15 +44,20 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class JsonResponseConfig {
+    private static final Logger logger = LoggerFactory.getLogger(JsonResponseConfig.class);
 
     private static final DateTimeFormatter LOCAL_DATE_TIME = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
     private static final DateTimeFormatter LOCAL_DATE = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private static final DateTimeFormatter LOCAL_TIME = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
     private List<JacksonObjectMapperBuilderConfigure> configurers = new ArrayList<JacksonObjectMapperBuilderConfigure>();
+
+    @Value("${com.guns21.spring.mvc.valuable-enum-package:#{null}}")
+    private String valuableEnumPackage;
 
     @Autowired(required = false)
     public void setConfigurers(List<JacksonObjectMapperBuilderConfigure> configurers) {
@@ -56,7 +70,33 @@ public class JsonResponseConfig {
         MappingJackson2HttpMessageConverter jsonConverter = new JsonResponseAwareJsonMessageConverter(objectMapper);
 
 //        objectMapper.registerModule(new JavaTimeModule());
+        registerEnumConverter(objectMapper);
         return jsonConverter;
+    }
+
+    private void registerEnumConverter(ObjectMapper objectMapper) {
+        SimpleModule module = new SimpleModule();
+
+        final ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+
+        // Add include filters which matches all the classes (or use your own)
+        provider.addIncludeFilter(new AssignableTypeFilter(ValuableEnum.class));
+
+        // Aet matching classes defined in the package
+        final Set<BeanDefinition> enumClasses = provider.findCandidateComponents(valuableEnumPackage);
+
+        enumClasses.forEach(beanDefinition -> {
+            try {
+                Class<Object> clazz = (Class<Object>) Class.forName(beanDefinition.getBeanClassName());
+                if (ValuableEnum.class.isAssignableFrom(clazz) && Enum.class.isAssignableFrom(clazz)) {
+                    module.addDeserializer(clazz, new ValuableEnumDeserializer(clazz));
+                }
+            } catch (ClassNotFoundException e) {
+                logger.error("class not found", e);
+            }
+        });
+
+        objectMapper.registerModule(module);
     }
 
     @Bean
