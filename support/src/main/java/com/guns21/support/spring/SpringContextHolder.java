bcda16dd2f4514b39e9c4fe6f85f10f804dfa527
jliu <liujun@ktjr.com>
2021-05-10 14:13:11 +0800

    feature:add holder for spring context

---
+++ support/src/main/java/com/guns21/support/spring/SpringContextHolder.java
@@ -0,0 +1,24 @@
+package com.guns21.support.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jliu
+ * @date 2021/5/7 6:27 下午
+ */
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
